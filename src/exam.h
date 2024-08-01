c2507d381ceee9bb064c4c52c3de6146bc009f15
vlopezvilla <153048628+vlopezvilla@users.noreply.github.com>
2024-08-01 14:40:30 -0700

    Create exam.h

---
+++ src/exam.h
@@ -0,0 +1,15 @@
+// exam.h
+#ifndef EXAM_H
+#define EXAM_H
+#include <iostream>
+
+// Declaration of the functions
+void examQuestions(const std::string& className); 
+void question1(const std::string& className);
+void question2(const std::string& className);
+void question3(const std::string& className);
+void question4(const std::string& className);
+void question5(const std::string& className);
+
+
+#endif
