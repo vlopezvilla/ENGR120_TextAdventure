07369634cf0d055318d4ba81066c84fb629d7647
vlopezvilla <153048628+vlopezvilla@users.noreply.github.com>
2024-08-01 14:39:52 -0700

    Create lecture.h

---
+++ src/lecture.h
@@ -0,0 +1,15 @@
+#ifndef LECTURE_H
+#define LECTURE_H
+#include <iostream>
+
+void attendLecture(const std::string& className, int testReadinessScore);
+void firstPart(const std::string& className);
+void secondPart(const std::string& className);
+void thirdPart(const std::string& className);
+void fourthPart(const std::string& className);
+void fifthPart(const std::string& className);
+void reviewLecture(const std::string& className, int testReadinessScore);
+int returnTestReadinessScore();
+
+
+#endif
