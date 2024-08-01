5537abd387cfae9ecb2921f604caa884890b67f0
vlopezvilla <153048628+vlopezvilla@users.noreply.github.com>
2024-08-01 14:38:21 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include "game.h"
 
 int main() {
-    std::cout << "Welcome to the Text Adventure Game!\n"; //We can give it a more creative title lol
+    std::cout << "Welcome to the University Student Text Game!\n"; //We can give it a more creative title lol
     std::cout << "To start the game choose your major.\n\n";
 
     // Start the game
     startGame();
 
-    std::cout << "Thank you for playing!\n";
+    std::cout << "\n\nThank you for playing!\n";
     return 0;
 }
