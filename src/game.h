7cba1628a329df4447d29362bb0a12131759a595
vlopezvilla <153048628+vlopezvilla@users.noreply.github.com>
2024-08-01 14:38:03 -0700

    Update game.h

--- src/game.h
+++ src/game.h
@@ -2,7 +2,7 @@
 #define GAME_H
 
 #include "player.h"
-#include "Schedule.h"
+#include "schedule.h"
 
 // Function Prototypes
 
@@ -13,9 +13,11 @@ void startGame();
 void chooseMajor(Player& player, Schedule& schedule);
 
 // Handles the scenario where the player wakes up and makes choices affecting their day.
-void wakingUpScenario(Player& player, Schedule& schedule);
+void wakingUpScenario(Player& player, Schedule& schedule, int testReadinessScore);
 
 // Randomly selects two classes for the player to attend and processes the outcomes.
-void goToClassScenario(Player &player, Schedule &schedule);
+void goToClassScenario(Player &player, Schedule &schedule, int testReadinessScore);
+
+void giveExam(const std::string& className, int testReadinessScore);
 
 #endif // GAME_H
